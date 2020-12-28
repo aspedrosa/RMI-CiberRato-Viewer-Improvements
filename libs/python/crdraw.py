e4650677ce8138d1c2231816c251d1117847e0ab
aspedrosa <aspedrosa@ua.pt>
2020-12-28 16:05:11 +0000

    add id+ttl on methods

--- libs/python/crdraw.py
+++ libs/python/crdraw.py
@@ -13,14 +13,12 @@ class CRDraw:
         self.sock = socket.socket(socket.AF_INET,  # Internet
                                   socket.SOCK_DGRAM)  # UDP
 
-        msg = '<Robot Id="' + str(robId) + '" Name="' + robName + '" />'
-
-        self.sock.sendto(msg.encode(), (host, port))
-
         self.color = (0, 0, 0)
 
         self.buffered = True
         self.buffer = []
+
+        self.ttl = 5000
 
     def bufferOnOff(self, on_off):
         self.buffered = on_off
@@ -40,39 +38,43 @@ class CRDraw:
     def setColor(self, r, g, b):
         self.color = (r, g, b)
 
-    def drawEllipse(self, diam_vertical, diam_horizontal, x, y, color: Tuple[int, int, int]=None):
+    def drawEllipse(self, id, diam_vertical, diam_horizontal, x, y, color: Tuple[int, int, int]=None, ttl: int=None):
         r, g, b = self.color if color is None else color
-        msg = f'<Ellipse red="{r}" green="{g}" blue="{b}" Diam_vertical="{diam_vertical}" Diam_horizontal="{diam_horizontal}" x="{x}" y="{y}"></Ellipse>'
+        ttl = self.ttl if ttl is None else ttl
+        msg = f'<Ellipse ttl="{ttl}" id="{id}" red="{r}" green="{g}" blue="{b}" Diam_vertical="{diam_vertical}" Diam_horizontal="{diam_horizontal}" x="{x}" y="{y}"></Ellipse>'
         if self.buffered:
             self.buffer.append(msg)
         else:
             self._sendMessage(msg)
 
-    def drawText(self, text, color=None):
+    def drawText(self, id, text, color=None, ttl=None):
         r, g, b = self.color if color is None else color
-        msg = f'<Text red="{r}" green="{g}" blue="{b}" text="{text}"></Text>'
+        ttl = self.ttl if ttl is None else ttl
+        msg = f'<Text ttl="{ttl}" id="{id}" red="{r}" green="{g}" blue="{b}" text="{text}"></Text>'
         if self.buffered:
             self.buffer.append(msg)
         else:
             self._sendMessage(msg)
 
-    def drawCircle(self, diam, x, y, color=None):
-        self.drawEllipse(diam, diam, x, y, color)
+    def drawCircle(self, id, diam, x, y, color=None, ttl=None):
+        self.drawEllipse(id, diam, diam, x, y, color, ttl)
 
-    def drawRectangle(self, width, height, x, y, color=None):
+    def drawRectangle(self, id, width, height, x, y, color=None, ttl=None):
         r, g, b = self.color if color is None else color
-        msg = f'<Rectangle red="{r}" green="{g}" blue="{b}" Width="{width}" Height="{height}" x="{x}" y="{y}"></Rectangle>'
+        ttl = self.ttl if ttl is None else ttl
+        msg = f'<Rectangle ttl="{ttl}" id="{id}" red="{r}" green="{g}" blue="{b}" Width="{width}" Height="{height}" x="{x}" y="{y}"></Rectangle>'
         if self.buffered:
             self.buffer.append(msg)
         else:
             self._sendMessage(msg)
 
-    def drawSquare(self, width, x, y, color=None):
-        self.drawRectangle(width, width, x, y, color)
+    def drawSquare(self, id, width, x, y, color=None, ttl=None):
+        self.drawRectangle(id, width, width, x, y, color, ttl)
 
-    def drawPolygon(self, point_list: List[Tuple[int, int]], color=None):
+    def drawPolygon(self, id, point_list: List[Tuple[int, int]], color=None, ttl=None):
         r, g, b = self.color if color is None else color
-        msg = [f'<Polygon red="{r}" green="{g}" blue="{b}">']
+        ttl = self.ttl if ttl is None else ttl
+        msg = [f'<Polygon ttl="{ttl}" id="{id}" red="{r}" green="{g}" blue="{b}">']
         for point in point_list:
             msg.append(f'<Point x="{point[0]}" y="{point[1]}"></Point>')
         msg.append(f'</Polygon>')
@@ -82,24 +84,25 @@ class CRDraw:
         else:
             self._sendMessage(msg)
 
-    def drawLine(self, x0, y0, x1, y1, color=None):
+    def drawLine(self, id, x0, y0, x1, y1, color=None, ttl=None):
         r, g, b = self.color if color is None else color
-        msg = f'<Line red="{r}" green="{g}" blue="{b}" x0="{x0}" y0="{y0}" x1="{x1}" y1="{y1}"></Line>'
+        ttl = self.ttl if ttl is None else ttl
+        msg = f'<Line ttl="{ttl}" id="{id}" red="{r}" green="{g}" blue="{b}" x0="{x0}" y0="{y0}" x1="{x1}" y1="{y1}"></Line>'
         if self.buffered:
             self.buffer.append(msg)
         else:
             self._sendMessage(msg)
 
-    def drawLine2(self, p0: Tuple[int, int], p1: Tuple[int, int], color=None):
-        self.drawLine(*p0, *p1, color)
+    def drawLine2(self, id, p0: Tuple[int, int], p1: Tuple[int, int], color=None, ttl=None):
+        self.drawLine(id, *p0, *p1, color, ttl)
 
 
 if __name__ == "__main__":
     crd = CRDraw("ola", 1)
     crd.bufferOnOff(False)
     points = [(0, 50), (400, 60), (50, 30), (700, 200)]
-    crd.drawPolygon(points, (0,0,255))
-    crd.drawLine2((700, 200), (850, 250), (255, 0, 0))
-    crd.drawSquare(20, 700, 200, (255, 255, 0))
-    crd.drawRectangle(50, 20, 700, 200, (255, 0, 0))
+    crd.drawPolygon(1, points, (0, 0, 255))
+    crd.drawLine2(2, (700, 200), (850, 250), (255, 0, 0), 20000)
+    crd.drawSquare(3, 20, 700, 200, (255, 255, 0), 30000)
+    crd.drawRectangle(4, 50, 20, 700, 200, (255, 0, 0), 2500)
     crd.drawAll()
